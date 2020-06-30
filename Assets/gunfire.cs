64be88595cb4a1a53c6741fd52a933fe252f9a07
Leonardo Price <leonardo.price@gmail.com>
2020-06-30 17:08:06 -0400

    gun anim

---
+++ Assets/gunfire.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunfire : MonoBehaviour
+{
+    private Animator gunAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gunAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("fire");
+            gunAnim.SetTrigger("fire");
+            //gunAnim.ResetTrigger("fire");
+        }
+    }
+}
